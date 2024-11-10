75de4f5fd88b236f1ce709bc049f08973364f53d
gohackathon2017 <gohackathon6@gmail.com>
2024-11-11 00:26:21 +0530

    Update apps.py

--- Server/Manual_attendance/Phase2/apps.py
+++ Server/Manual_attendance/Phase2/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Phase2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Manual_attendnance.Phase2'
+    name = 'Manual_attendance.Phase2'
