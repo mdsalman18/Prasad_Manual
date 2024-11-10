87b01b2d56850c5f5193dd4073f58c14215f8845
gohackathon2017 <gohackathon6@gmail.com>
2024-11-11 01:08:27 +0530

    Update asgi.py

--- Server/Manual_attendance/Manual_attendance/asgi.py
+++ Server/Manual_attendance/Manual_attendance/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Manual_attendance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Manual_attendance.Manual_attendance.settings')
 
 application = get_asgi_application()
